f61355dc250fdb806905af4785b5727e7bddaf87
PainIam <tensie334@gmail.com>
2025-12-03 23:09:52 +0200

    Adding fen string generation

--- src/board.cpp
+++ src/board.cpp
@@ -2,7 +2,7 @@
 
 
 
-Board::Board() {
+Board::Board() : m_toMove(1) {
     // create temp variable which should nicely initialize board
     int start[BOARD_SIZE] = {
     brook, bknight, bbishop, bqueen, bking, bbishop, bknight, brook, off, off, off, off, off, off, off, off,
@@ -18,6 +18,52 @@ Board::Board() {
     // initialize board with start
     for (int i = 0; i < BOARD_SIZE; i++) {
         board[i] = start[i];
+    }
+
+    
+
+}
+
+std::string Board::getFen() {
+    std::string fen = "";
+
+    int rank_inc = 7;
+    int curr_sq = 0;
+    int empties = 0; // to empties empty squares
+ 
+    while(curr_sq <= 119) {
+        if ((curr_sq & 0x88) != 0) {
+            if (empties != 0) {
+                fen += std::to_string(empties); // pb empties of empty squares if we're at end of rank
+                empties = 0; // reset the emptieser
+            }
+            curr_sq += rank_inc; // jump down to the the next rank
+            if (curr_sq != 112) fen += "/";
+        } else { // valid board square
+            if (board[curr_sq] != empty) { // we enemptiesered a piece 
+                if (empties != empty) fen += std::to_string(empties);
+                empties = 0; // reset the empties emptieser
+            }
+            // if not let's get the character at the board
+            switch(board[curr_sq]) {
+					case wking  : fen += "K";	break;
+					case wqueen : fen += "Q";   break;
+					case wrook  : fen += "R";	break;
+					case wbishop: fen += "B";   break;
+					case wknight: fen += "N";   break;
+					case wpawn  : fen += "P";   break;
+					case bking  : fen += "k";	break;
+					case bqueen : fen += "q";   break;
+					case brook  : fen += "r";   break;
+					case bbishop: fen += "b";   break;
+					case bknight: fen += "n";   break;
+					case bpawn  : fen += "p";	break;
+                    default     : empties++; // increment the empty square emptieser if 
+            }
+
+        }
+        curr_sq++; // proceed to next square.
+
     }
 
 }
--- src/board.h
+++ src/board.h
@@ -2,15 +2,25 @@
 #include <iostream>
 
 #define BOARD_SIZE 128
+#define WHITE_MOVE 1
+#define BLACK_MOVE -1
+#define INI_FEN "rnbqkbnr/ppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 #define endl "\n"
 
-enum {empty, wpawn, wrook, wknight, wbishop, wqueen, wking, bpawn, brook, bknight, bbishop, bqueen, bking, off};
+enum Pieces {empty, wpawn, wrook, wknight, wbishop, wqueen, wking, bpawn, brook, bknight, bbishop, bqueen, bking, off};
+
 
 
 class Board {
     private:
         int board[BOARD_SIZE];
+        int m_toMove;
+        int m_enPas;
+        int m_Ply;
+        int m_HisPly;
+        int m_PosKey;
     public:
         Board();
         void print() const;
+        std::string getFen();
 };
